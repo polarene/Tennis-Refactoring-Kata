2882da90b8133addc67b82960e465ddeb4420229
mmirk <t-mmirk@hotels.com>
2022-03-22 22:25:39 +0100

    [WIP] Wrap All Primitives And Strings

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -1,18 +1,27 @@
 class TennisGame1(private val player1Name: String, private val player2Name: String) : TennisGame {
 
-    private var m_score1: Int = 0
-    private var m_score2: Int = 0
+    private var m_score1 = Score()
+    private var m_score2 = Score()
 
     override fun wonPoint(playerName: String) {
         if (playerName === player1Name) {
-            m_score1 += 1
+            m_score1++
             return
         }
-        m_score2 += 1
+        m_score2++
     }
 
     override fun getScore(): String {
-        if (m_score1 == m_score2) {
+        TODO()
+    }
+
+}
+
+data class Score(private var score: Int = 0) {
+    operator fun inc() = apply { score++ }
+
+    fun relativeTo(other: Score): String {
+        if (this == other) {
             return parity()
         }
         if (m_score1 >= 4 || m_score2 >= 4) {
@@ -22,14 +31,6 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
     }
 
     private fun low() = "${lowScore(m_score1)}-${lowScore(m_score2)}"
-
-    private fun lowScore(score: Int) = when (score) {
-        0 -> "Love"
-        1 -> "Fifteen"
-        2 -> "Thirty"
-        3 -> "Forty"
-        else -> throw IllegalStateException("invalid score: $score")
-    }
 
     private fun advantageOrWin(): String {
         val difference = m_score1 - m_score2
@@ -46,5 +47,13 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
         1 -> "Fifteen-All"
         2 -> "Thirty-All"
         else -> "Deuce"
+    }
+
+    private fun lowScore() = when (score) {
+        0 -> "Love"
+        1 -> "Fifteen"
+        2 -> "Thirty"
+        3 -> "Forty"
+        else -> throw IllegalStateException("invalid score: $score")
     }
 }
