413d261ac9cada7145d57302e599b14a514108c2
mmirk <t-mmirk@hotels.com>
2022-03-25 18:15:26 +0100

    collective refactoring
    Co-authored-by:
    Matteo Mirk
    Oltion Doda
    Giorgio Vespucci
    Aarushi Agrawal
    Francesco Feltrinelli
    Davide Caruso
    Vincenza Cipriano
    Ruben Munoz Palomino
    Mark Kiraly
    Matthew O'Connor

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -1,51 +1,58 @@
 class TennisGame1(private val player1Name: String, private val player2Name: String) : TennisGame {
 
-    private var m_score1: Int = 0
-    private var m_score2: Int = 0
+    private var playerOneScore = PlayerScore()
+    private var playerTwoScore = PlayerScore()
 
     override fun wonPoint(playerName: String) {
-        if (playerName === "player1")
-            m_score1 += 1
+        if (playerName === player1Name)
+            playerOneScore++
         else
-            m_score2 += 1
+            playerTwoScore++
     }
 
     override fun getScore(): String {
-        var score = ""
-        var tempScore = 0
-        if (m_score1 == m_score2) {
-            when (m_score1) {
-                0 -> score = "Love-All"
-                1 -> score = "Fifteen-All"
-                2 -> score = "Thirty-All"
-                else -> score = "Deuce"
-            }
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            val minusResult = m_score1 - m_score2
-            if (minusResult == 1)
-                score = "Advantage player1"
-            else if (minusResult == -1)
-                score = "Advantage player2"
-            else if (minusResult >= 2)
-                score = "Win for player1"
-            else
-                score = "Win for player2"
-        } else {
-            for (i in 1..2) {
-                if (i == 1)
-                    tempScore = m_score1
-                else {
-                    score += "-"
-                    tempScore = m_score2
-                }
-                when (tempScore) {
-                    0 -> score += "Love"
-                    1 -> score += "Fifteen"
-                    2 -> score += "Thirty"
-                    3 -> score += "Forty"
-                }
-            }
+        if (playerOneScore == playerTwoScore) {
+            return parityScore()
         }
-        return score
+        if (playerOneScore.score >= 4 || playerTwoScore.score >= 4) {
+            return advantageOrWinScore()
+        }
+        return loveToForty()
+    }
+
+    private fun loveToForty(): String {
+        return "${scoreLabel(playerOneScore.score)}-${scoreLabel(playerTwoScore.score)}"
+    }
+
+    private fun scoreLabel(score: Int) = when (score) {
+        0 -> "Love"
+        1 -> "Fifteen"
+        2 -> "Thirty"
+        3 -> "Forty"
+        else -> error("Invalid Score $score")
+    }
+
+    private fun advantageOrWinScore(): String {
+        val minusResult = playerOneScore.score - playerTwoScore.score
+        return when {
+            minusResult == 1 -> "Advantage player1"
+            minusResult == -1 -> "Advantage player2"
+            minusResult >= 2 -> "Win for player1"
+            else -> "Win for player2"
+        }
+    }
+
+    private fun parityScore() = when (playerOneScore.score) {
+        0 -> "Love-All"
+        1 -> "Fifteen-All"
+        2 -> "Thirty-All"
+        else -> "Deuce"
+    }
+}
+
+data class PlayerScore(var score: Int = 0) {
+    operator fun inc(): PlayerScore {
+        this.score++
+        return this
     }
 }
